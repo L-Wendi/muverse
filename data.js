9be61aeae778066a7484eb19432dbb50ebbc2f7a
sam-min <smriti.minhas@gmail.com>
2023-11-19 12:15:52 -0800

    I currently have 13 songs in this file.

--- data.js
+++ data.js
@@ -1,7 +1,13 @@
   
 /*  1. Despacito
     2. Heal the world
-    3. */
+    3. Jai Ho
+    4. Waka waka (This time for Africa)
+    5. La Bamba
+    6. Tulpen uit Amsterdam
+    7. Wangeci
+    8. Jupiter, the Bringer of Jollity
+    9. */
     
     const despacitoInfo = {
         song: {
@@ -24,9 +30,9 @@
         artistMessage: "The song 'Despacito' celebrates the joy of life, love, and the vibrant culture of Puerto Rico. It encourages people to enjoy the moment and dance to the rhythm of life."
       };
       
-      console.log(despacitoInfo);
+    console.log(despacitoInfo);
 
-      const healTheWorldInfo = {
+    const healTheWorldInfo = {
         song: {
           title: "Heal the World",
           genre: "Pop",
@@ -41,10 +47,107 @@
         artistMessage: "In 'Heal the World,' Michael Jackson sends a powerful message of compassion and hope. The song encourages listeners to contribute to making the world a better place through love, understanding, and caring for one another, particularly focusing on the well-being of children and the vulnerable."
       };
       
-      console.log(healTheWorldInfo);
+    console.log(healTheWorldInfo);
       
+    const jaiHoInfo = {
+        song: {
+          title: "Jai Ho",
+          genre: "Bollywood/Pop",
+          language: "Hindi/Urdu",
+          occasion: "Celebrations, Victory, Festivals",
+          culture: "Indian",
+          artistMessage: "In 'Jai Ho,' A.R. Rahman delivers a message of triumph and celebration. The song, known for its infectious energy, encourages people to rejoice in moments of victory and embrace the joy of life. With a blend of Bollywood and pop elements, 'Jai Ho' has become an anthem of positivity and exuberance."
+        },
+        artist: {
+          name: "A.R. Rahman",
+          homeland: "India"
+        }
+      };
+      
+    console.log(jaiHoInfo);
+      
+    const wakaWakaInfo = {
+        song: {
+          title: "Waka Waka (This Time for Africa)",
+          genre: "Afrobeat/Pop",
+          language: "English/Spanish",
+          occasion: "Sports events, Celebrations, Unity",
+          culture: "African/Colombian",
+          artistMessage: "In 'Waka Waka,' Shakira sends a message of unity, celebration, and empowerment. The song, chosen as the official song for the 2010 FIFA World Cup, embodies the spirit of Africa and encourages people to come together, celebrate diversity, and showcase the vibrancy of African and Colombian cultures through music and dance."
+        },
+        artist: {
+          name: "Shakira",
+          homeland: "Colombia"
+        }
+      };
+      
+    console.log(wakaWakaInfo);
+      
+    const laBambaInfo = {
+        song: {
+          title: "La Bamba",
+          genre: "Rock and Roll",
+          language: "Spanish",
+          occasion: "Celebrations, Dance parties",
+          culture: "Mexican-American",
+          artistMessage: "In 'La Bamba,' Ritchie Valens celebrates Mexican-American culture and music. The song, a classic in the rock and roll genre, invites people to dance and enjoy the lively rhythms of traditional Mexican music. With its catchy beat and infectious energy, 'La Bamba' has become an iconic representation of cross-cultural fusion in the world of music."
+        },
+        artist: {
+          name: "Ritchie Valens",
+          homeland: "United States (Mexican-American)"
+        }
+      };
+      
+    console.log(laBambaInfo);
+      
+    const tulpenUitAmsterdamInfo = {
+        song: {
+          title: "Tulpen uit Amsterdam",
+          genre: "Dutch",
+          language: "Dutch",
+          occasion: "Dutch celebrations, Festivals",
+          culture: "Dutch",
+          artistMessage: "In 'Tulpen uit Amsterdam,' Herman Emmink celebrates the beauty of tulips and the city of Amsterdam. The song, sung in Dutch, is often associated with Dutch traditions and festivities. With its cheerful melody, the song reflects the joy and pride of the Dutch culture, particularly the iconic tulip fields and the vibrant city of Amsterdam."
+        },
+        artist: {
+          name: "Herman Emmink",
+          homeland: "Netherlands"
+        }
+      };
+      
+    console.log(tulpenUitAmsterdamInfo);
     
-    
-
-
-    
+    const wangeciInfo = {
+        song: {
+          title: "Wangeci",
+          genre: "Benga (Kenyan)",
+          language: "Luo",
+          occasion: "Traditional ceremonies, Celebrations",
+          culture: "Kenyan",
+          artistMessage: "In 'Wangeci,' Sukuma Bin Ongaro celebrates the Kenyan Luo culture. The song, sung in Luo, reflects the vibrancy and richness of Kenyan traditional music. 'Wangeci' is often played during traditional ceremonies and celebrations, serving as a testament to the cultural heritage and diversity of Kenya. The artist's message through the song is a celebration of love, life, and the spirit of the Kenyan people."
+        },
+        artist: {
+          name: "Sukuma Bin Ongaro",
+          homeland: "Kenya"
+        }
+      };
+      
+    console.log(wangeciInfo);
+      
+    const jupiterInfo = {
+        song: {
+          title: "Jupiter, the Bringer of Jollity",
+          genre: "Classical",
+          language: "Instrumental (No lyrics)",
+          occasion: "Concerts, Formal events",
+          culture: "Western Classical",
+          artistMessage: "In 'Jupiter, the Bringer of Jollity,' Gustav Holst creates a musical masterpiece that is part of his larger composition 'The Planets.' This instrumental piece, without lyrics, is a part of Western Classical music, known for its grandeur and emotional depth. Often played in concert settings and formal events, the music captures the essence of Jupiter, the Roman god of thunder and the bringer of joy. Holst's message through this piece lies in the evocation of a majestic and jubilant atmosphere, celebrating the power and vitality associated with the planet Jupiter."
+        },
+        artist: {
+          name: "Gustav Holst",
+          homeland: "United Kingdom"
+        }
+      };
+      
+    console.log(jupiterInfo);
+      
